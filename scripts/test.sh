246bf834d4bef09e63124f83aaf9da2880cebcc3
eduardo aleixo <eduardoaleixomartins@gmail.com>
2020-02-08 14:52:53 -0300

    chore(tests): give exec permission to bash scripts
    bats needs those files to be executable
    
    also this will be needed once we make the ci run them

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,13 @@ set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
-find "$DIR/../content" -type f -name "*.bats" -exec \
-	npm run bats --prefix "$DIR/.." {} +
+test_bash() {
+	# Bats need the files to have execute permission
+	find "$DIR/../content" -type f -name "*.sh" -exec \
+		chmod +x {} +
 
+	find "$DIR/../content" -type f -name "*.bats" -exec \
+		 npm run bats --prefix "$DIR/.." {} +
+}
+
+test_bash

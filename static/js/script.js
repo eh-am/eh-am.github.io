4ab06655deaf6cbeda20c7bc218309bdc1d54af7
eduardo aleixo <eduardoaleixomartins@gmail.com>
2021-02-13 12:31:01 -0300

    feat(page-transition): smooth out the transition

--- static/js/script.js
+++ static/js/script.js
@@ -6,16 +6,32 @@ function getPageTransitioner() {
   return document.getElementById("page-transitioner");
 }
 
+function calculateDesiredDiamater() {
+  // motivation:
+  // * for the circle animation to cover the whole screen,
+  // its radius needs to be the same as the hypothenuse
+  // * we can only set the diamater, due to css circle contraints
+  // * radius = diameter/2
+  // starting with the pythagorean theorem:
+  // hypotenuse^2 = width^2 + height^2
+  // hypotenuse = radius = diameter/2
+  // (diam/2)^2 = w^2 + h^2
+  // d = 2 * sqrt(w^2 + h^2)
+  return 2 * Math.sqrt(Math.pow(window.innerHeight,2) + Math.pow(window.innerWidth, 2));
+}
+
 // TODO: what happens if two links are clicked at the same time?
 function addAnimationToAnchor(anchor) {
   // TODO: other events
   anchor.addEventListener("click", (event) => {
-    const pt = getPageTransitioner();
-
     // delay going to new url
     event.preventDefault();
 
+    const pt = getPageTransitioner();
+    const desiredDiameter = calculateDesiredDiamater();
+
     // start animation
+    pt.style.setProperty("--pageTransitionDiameter", desiredDiameter);
     pt.classList.add("start");
 
     // go to link after animation ends

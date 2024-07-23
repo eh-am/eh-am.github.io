3755833922bb1407cf10ad922be82b5d99465990
Eduardo Aleixo <eduardo@carpa.dev>
2024-07-23 12:14:04 +0100

    sort by date

--- scripts/weekly-digest/group.go
+++ scripts/weekly-digest/group.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type GroupedItems struct {
 	Year       string `json:"year"`
@@ -28,6 +31,12 @@ func GroupByISOWeek(items map[string]Item) map[string]GroupedItems {
 
 		group := res[key]
 		group.Items = append(group.Items, item)
+
+		// This makes it definitely slower
+		sort.Slice(group.Items, func(i, j int) bool {
+			// TODO: maybe we should do something smart like time updated?
+			return group.Items[i].TimeAdded.Time.Before(group.Items[j].TimeAdded.Time)
+		})
 
 		// Since we get a copy back, let's write it back to the map
 		res[key] = group
--- scripts/weekly-digest/write.go
+++ scripts/weekly-digest/write.go
@@ -19,6 +19,7 @@ func Write(dest string, groups map[string]GroupedItems) error {
 		}
 
 		// Pad since we gonna sort using the file system
+		// TODO: check if file already exists and only add new items?
 		padded := fmt.Sprintf("%s-%02s", g.Year, g.WeekNumber)
 		err = ioutil.WriteFile(filepath.Join(dest, padded+".json"), file, 0644)
 		if err != nil {
